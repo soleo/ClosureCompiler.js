2b9d4e499e4709f0528100f9889a3683984c8917
Xinjiang Shao <xinjiang.shao@peapod.com>
2020-02-26 12:07:52 -0600

    Add a different url

--- scripts/configure.js
+++ scripts/configure.js
@@ -31,7 +31,7 @@ var ClosureCompiler = require(__dirname+"/../ClosureCompiler.js"),
 console.log("Configuring ClosureCompiler.js "+pkg.version+" ...\n");
 
 // Closure Compiler download url
-var ccUrl = "https://github.com/soleo/google-closure-compiler/raw/master/compiler-20200204.tar.gz";
+var ccUrl = "https://raw.githubusercontent.com/soleo/google-closure-compiler/master/compiler-20200204.tar.gz";
 
 // Temporary file for the download
 var ccTempFile = path.normalize(path.join(__dirname, "..", "compiler", "compiler.tar.gz"));
